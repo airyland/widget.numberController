3322e237b88b9ec96d717f8ec6de8ad1c37f5cc5
airyland <airyland@qq.com>
2013-12-06 14:57:07 +0800

    update checking

--- dist/numberController-debug.js
+++ dist/numberController-debug.js
@@ -1,110 +1,131 @@
-define("seedit/numberController/0.0.1/numberController-debug", [ "$-debug" ], function(require, exports, module) {
+define("seedit/numberController/0.0.1/numberController-debug", [], function(require, exports, module) {
     //@todo bash bind
-    var $ = require("$-debug");
-    var numberController = function(option) {
-        var _this = this;
-        var defaults = {
-            plusEle: null,
-            minusEle: null,
-            numberEle: null,
-            minusDisabledClass: "minus-disabled",
-            plusDisabledClass: "plus-disabled",
-            minNumber: 0,
-            step: 1,
-            onchange: $.noop
+    //@todo number custom getter and setter
+    //@todo code cleaning
+    var numberController;
+    (function($) {
+        numberController = function(option) {
+            var _this = this;
+            var defaults = {
+                plusEle: null,
+                minusEle: null,
+                numberEle: null,
+                minusDisabledClass: "minus-disabled",
+                plusDisabledClass: "plus-disabled",
+                minNumber: 0,
+                step: 1,
+                onchange: $.noop,
+                valueAdapter: {
+                    get: $.noop,
+                    set: $.noop
+                }
+            };
+            $.extend(defaults, option);
+            this.defaults = function() {
+                return defaults;
+            }();
+            if (!_this.defaults.minusEle && !_this.defaults.numberEle) throw "shit! No number element specified";
+            $.each([ "$numberEle", "$plusEle", "$minusEle" ], function(index, value) {
+                _this[value] = $(_this.defaults[value.slice(1)]).eq(0);
+            });
+            return this.init(option);
         };
-        $.extend(defaults, option);
-        this.defaults = function() {
-            return defaults;
-        }();
-        if (!_this.defaults.minusEle && !_this.defaults.numberEle) throw "shit! No number element specified";
-        $.each([ "$numberEle", "$plusEle", "$minusEle" ], function(index, value) {
-            _this[value] = $(_this.defaults[value.slice(1)]).eq(0);
-        });
-        return this.init(option);
-    };
-    numberController.prototype = {
-        init: function(option) {
-            var _this = this;
-            _this.$minusEle.click(function() {
-                _this.minus(_this.defaults.step);
-            });
-            _this.$plusEle.click(function() {
-                _this.plus(_this.defaults.step);
-            });
-            _this.minus(0);
-            _this.plus(0);
-            return _this;
-        },
-        minus: function(step) {
-            var _this = this;
-            var currentNo = this.val();
-            currentNo -= step;
-            this.val(currentNo);
-            if (step !== 0) {
-                this.emit("change");
-            }
-            if ($.isNumeric(_this.defaults.minNumber)) {
-                if (_this.defaults.minNumber === this.val()) {
-                    setTimeout(function() {
-                        _this.$minusEle.prop("disabled", true).addClass(_this.defaults.minusDisabledClass);
-                    }, 0);
-                    return;
+        numberController.prototype = {
+            init: function(option) {
+                var _this = this;
+                _this.$minusEle.click(function() {
+                    if ($(this).hasClass(_this.defaults.minusDisabledClass)) return;
+                    _this.minus(_this.defaults.step);
+                });
+                _this.$plusEle.click(function() {
+                    if ($(this).hasClass(_this.defaults.plusDisabledClass)) return;
+                    _this.plus(_this.defaults.step);
+                });
+                _this.minus(0);
+                _this.plus(0);
+                _this.$numberEle.on("change blur", function() {
+                    _this.check();
+                });
+                return _this;
+            },
+            check: function() {
+                if (this.$numberEle.val() * 1 === this.defaults.minNumber) {
+                    this.$minusEle.addClass(this.defaults.minusDisabledClass);
                 }
-            }
-        },
-        plus: function(step) {
-            var _this = this;
-            var currentNo = this.val();
-            currentNo += step;
-            this.val(currentNo);
-            if (step !== 0) {
-                this.emit("change");
-            }
-            if ($.isNumeric(_this.defaults.maxNumber)) {
-                if (_this.defaults.maxNumber === this.val()) {
-                    setTimeout(function() {
-                        _this.$plusEle.prop("disabled", true).addClass(_this.defaults.plusDisabledClass);
-                    }, 0);
+                if (this.$numberEle.val() * 1 === this.defaults.maxNumber) {
+                    this.$plusEle.addClass(this.defaults.plusDisabledClass);
                 }
-            }
-        },
-        // getter and setter
-        val: function(number) {
-            var _this = this;
-            if ($.isNumeric(number)) {
-                // check min and max
+            },
+            minus: function(step) {
+                var _this = this;
+                var currentNo = this.val();
+                currentNo -= step;
+                this.val(currentNo);
+                if (step !== 0) {
+                    this.emit("change");
+                }
                 if ($.isNumeric(_this.defaults.minNumber)) {
-                    if (number > _this.defaults.minNumber) {
-                        this.$numberEle.val(number);
-                        if (_this.$plusEle.is(":disabled")) {
-                            _this.$plusEle.prop("disabled", false).removeClass(_this.defaults.plusDisabledClass);
-                        }
-                    } else {
-                        this.$minusEle.prop("disabled", true);
+                    if (_this.defaults.minNumber === this.val()) {
+                        setTimeout(function() {
+                            _this.$minusEle.prop("disabled", true).addClass(_this.defaults.minusDisabledClass);
+                        }, 0);
+                        return;
                     }
+                }
+            },
+            plus: function(step) {
+                var _this = this;
+                var currentNo = this.val();
+                currentNo += step;
+                this.val(currentNo);
+                if (step !== 0) {
+                    this.emit("change");
                 }
                 if ($.isNumeric(_this.defaults.maxNumber)) {
-                    if (number < _this.defaults.maxNumber) {
-                        this.$numberEle.val(number);
-                        if (_this.$minusEle.is(":disabled")) {
-                            _this.$minusEle.prop("disabled", false).removeClass(_this.defaults.minusDisabledClass);
-                        }
-                    } else {
-                        this.$plusEle.prop("disabled", true);
+                    if (_this.defaults.maxNumber === this.val()) {
+                        setTimeout(function() {
+                            _this.$plusEle.prop("disabled", true).addClass(_this.defaults.plusDisabledClass);
+                        }, 0);
                     }
                 }
-                return this;
-            } else {
-                return parseInt(this.$numberEle.val(), 10);
-            }
-        },
-        emit: function(event, value, callback) {
-            if (event === "change") {
-                this.defaults.onchange && this.defaults.onchange(this.val());
-            }
-        },
-        on: function(event, value, callback) {}
-    };
+            },
+            // getter and setter
+            val: function(number) {
+                var _this = this;
+                if ($.isNumeric(number)) {
+                    // check min and max
+                    if ($.isNumeric(_this.defaults.minNumber)) {
+                        if (number > _this.defaults.minNumber) {
+                            this.$numberEle.val(number);
+                            if (_this.$plusEle.is(":disabled") || _this.$plusEle.hasClass(_this.defaults.plusDisabledClass)) {
+                                _this.$plusEle.prop("disabled", false).removeClass(_this.defaults.plusDisabledClass);
+                            }
+                        } else {
+                            this.$minusEle.prop("disabled", true);
+                        }
+                    }
+                    if ($.isNumeric(_this.defaults.maxNumber)) {
+                        if (number < _this.defaults.maxNumber) {
+                            this.$numberEle.val(number);
+                            if (_this.$minusEle.is(":disabled") || _this.$minusEle.hasClass(_this.defaults.minusDisabledClass)) {
+                                _this.$minusEle.prop("disabled", false).removeClass(_this.defaults.minusDisabledClass);
+                            }
+                        } else {
+                            this.$plusEle.prop("disabled", true);
+                        }
+                    }
+                    return this;
+                } else {
+                    return parseInt(this.$numberEle.val(), 10);
+                }
+            },
+            emit: function(event, value, callback) {
+                if (event === "change") {
+                    this.defaults.onchange && this.defaults.onchange(this.val());
+                }
+            },
+            on: function(event, value, callback) {}
+        };
+    })(jQuery);
     module.exports = numberController;
 });
--- src/numberController.js
+++ src/numberController.js
@@ -1,8 +1,10 @@
-define(function (require, exports, module) {
+define(function(require, exports, module) {
     //@todo bash bind
+    //@todo number custom getter and setter
+    //@todo code cleaning
     var numberController;
-    (function ($) {
-        numberController = function (option) {
+    (function($) {
+        numberController = function(option) {
             var _this = this;
             var defaults = {
                 plusEle: null,
@@ -12,32 +14,55 @@ define(function (require, exports, module) {
                 plusDisabledClass: 'plus-disabled',
                 minNumber: 0,
                 step: 1,
-                onchange: $.noop
+                onchange: $.noop,
+                valueAdapter: {
+                    get: $.noop,
+                    set: $.noop
+                }
             };
             $.extend(defaults, option);
-            this.defaults = (function () {
+            this.defaults = (function() {
                 return defaults;
             })();
             if (!_this.defaults.minusEle && !_this.defaults.numberEle) throw ('shit! No number element specified');
-            $.each(['$numberEle', '$plusEle', '$minusEle'], function (index, value) {
+            $.each(['$numberEle', '$plusEle', '$minusEle'], function(index, value) {
                 _this[value] = $(_this.defaults[value.slice(1)]).eq(0);
             });
             return this.init(option);
         };
         numberController.prototype = {
-            init: function (option) {
+            init: function(option) {
                 var _this = this;
-                _this.$minusEle.click(function () {
+                _this.$minusEle.click(function() {
+                    if ($(this).hasClass(_this.defaults.minusDisabledClass)) return;
                     _this.minus(_this.defaults.step);
                 });
-                _this.$plusEle.click(function () {
+                _this.$plusEle.click(function() {
+                    if ($(this).hasClass(_this.defaults.plusDisabledClass)) return;
                     _this.plus(_this.defaults.step);
                 });
                 _this.minus(0);
                 _this.plus(0);
+
+                _this.$numberEle.on('change blur', function() {
+                    _this.check();
+                });
                 return _this;
             },
-            minus: function (step) {
+            check: function() {
+                if (this.$numberEle.val() * 1 === this.defaults.minNumber) {
+                    this.$minusEle.addClass(this.defaults.minusDisabledClass);
+                } else {
+                    this.$minusEle.removeClass(this.defaults.minusDisabledClass);
+                }
+
+                if (this.$numberEle.val() * 1 === this.defaults.maxNumber) {
+                    this.$plusEle.addClass(this.defaults.plusDisabledClass);
+                } else {
+                    this.$plusEle.removeClass(this.defaults.plusDisabledClass);
+                }
+            },
+            minus: function(step) {
                 var _this = this;
                 var currentNo = this.val();
                 currentNo -= step;
@@ -47,14 +72,14 @@ define(function (require, exports, module) {
                 }
                 if ($.isNumeric(_this.defaults.minNumber)) {
                     if (_this.defaults.minNumber === this.val()) {
-                        setTimeout(function () {
+                        setTimeout(function() {
                             _this.$minusEle.prop('disabled', true).addClass(_this.defaults.minusDisabledClass);
                         }, 0);
                         return;
                     }
                 }
             },
-            plus: function (step) {
+            plus: function(step) {
                 var _this = this;
                 var currentNo = this.val();
                 currentNo += step;
@@ -64,24 +89,22 @@ define(function (require, exports, module) {
                 }
                 if ($.isNumeric(_this.defaults.maxNumber)) {
                     if (_this.defaults.maxNumber === this.val()) {
-                        setTimeout(function () {
+                        setTimeout(function() {
                             _this.$plusEle.prop('disabled', true).addClass(_this.defaults.plusDisabledClass);
-                            ;
                         }, 0);
                     }
                 }
             },
             // getter and setter
-            val: function (number) {
+            val: function(number) {
                 var _this = this;
                 if ($.isNumeric(number)) {
                     // check min and max
                     if ($.isNumeric(_this.defaults.minNumber)) {
                         if (number > _this.defaults.minNumber) {
                             this.$numberEle.val(number);
-                            if (_this.$plusEle.is(':disabled')) {
+                            if (_this.$plusEle.is(':disabled') || _this.$plusEle.hasClass(_this.defaults.plusDisabledClass)) {
                                 _this.$plusEle.prop('disabled', false).removeClass(_this.defaults.plusDisabledClass);
-                                ;
                             }
                         } else {
                             this.$minusEle.prop('disabled', true);
@@ -91,7 +114,7 @@ define(function (require, exports, module) {
                     if ($.isNumeric(_this.defaults.maxNumber)) {
                         if (number < _this.defaults.maxNumber) {
                             this.$numberEle.val(number);
-                            if (_this.$minusEle.is(':disabled')) {
+                            if (_this.$minusEle.is(':disabled') || _this.$minusEle.hasClass(_this.defaults.minusDisabledClass)) {
                                 _this.$minusEle.prop('disabled', false).removeClass(_this.defaults.minusDisabledClass);
                             }
                         } else {
@@ -104,12 +127,12 @@ define(function (require, exports, module) {
                 }
 
             },
-            emit: function (event, value, callback) {
+            emit: function(event, value, callback) {
                 if (event === 'change') {
                     this.defaults.onchange && this.defaults.onchange(this.val())
                 }
             },
-            on: function (event, value, callback) {
+            on: function(event, value, callback) {
 
             }
         };
